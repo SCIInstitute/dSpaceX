ddf0446d872efcb8cc6620a262b2caf65736d7c2
Jonathan Bronson <jonbronson@gmail.com>
2017-10-12 13:50:31 -0600

    Add test data and two starter unit tests.

--- test/lib/HDVizData_tests.cpp
+++ test/lib/HDVizData_tests.cpp
@@ -1,5 +1,22 @@
 #include "gtest/gtest.h"
+#include "FileCachedHDVizDataImpl.h"
+#include "SimpleHDVizDataImpl.h"
+#include "HDProcessResult.h"
 
-TEST(group1, test1) {
-	ASSERT_TRUE(true);
+#include <iostream>
+#include <exception>
+
+std::string data_dir = std::string(TEST_DATA_DIR);   
+
+TEST(FileCachedHDVizDataImpl, loadsFiles) {  
+  try {
+    HDVizData* data = new FileCachedHDVizDataImpl(data_dir);
+  } catch(std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
+
+TEST(SimpleHDVizDataImpl, loadData) {
+  HDProcessResult result;
+  HDVizData* data = new SimpleHDVizDataImpl(&result);
 }
