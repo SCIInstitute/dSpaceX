1afaf0dec20bae4255c482a24c5a7b34ab96cde1
Jon Bronson <jonbronson@gmail.com>
2018-01-22 16:15:20 -0700

    Fix matrix loader for non-square matrices.

--- lib/hdprocess/util/csv/loaders.cpp
+++ lib/hdprocess/util/csv/loaders.cpp
@@ -7,9 +7,9 @@ namespace HDProcess {
 
 FortranLinalg::DenseMatrix<Precision> loadCSVMatrix(std::string filename) {
   std::ifstream fstream(filename);
-  std::string line; 
+  std::string line;
   std::string token;
-  
+
   std::vector<std::vector<double>> matrix;
   while (std::getline(fstream, line)) {
     std::istringstream ss(line);
@@ -17,12 +17,12 @@ FortranLinalg::DenseMatrix<Precision> loadCSVMatrix(std::string filename) {
     while (std::getline(ss, token, ',')) {
       row.push_back(std::stod(token));
     }
-    matrix.push_back(row);    
+    matrix.push_back(row);
   }
-  
-  FortranLinalg::DenseMatrix<Precision> m(matrix.size(),matrix.size());
+
+  FortranLinalg::DenseMatrix<Precision> m(matrix.size(),matrix[0].size());
   for (int i = 0; i < matrix.size(); i++) {
-    for (int j = 0; j < matrix.size(); j++) {
+    for (int j = 0; j < matrix[0].size(); j++) {
       m(i,j) = matrix[i][j];
     }
   }
@@ -32,9 +32,9 @@ FortranLinalg::DenseMatrix<Precision> loadCSVMatrix(std::string filename) {
 
 FortranLinalg::DenseVector<Precision> loadCSVColumn(std::string filename) {
   std::ifstream fstream(filename);
-  std::string line; 
+  std::string line;
   std::string token;
-  
+
   std::vector<double> vector;
   while (std::getline(fstream, line)) {
     std::istringstream ss(line);
@@ -42,11 +42,11 @@ FortranLinalg::DenseVector<Precision> loadCSVColumn(std::string filename) {
     while (std::getline(ss, token, ',')) {
       row.push_back(std::stod(token));
     }
-    vector.push_back(row[0]);    
+    vector.push_back(row[0]);
   }
-  
+
   FortranLinalg::DenseVector<Precision> v(vector.size());
-  for (int i = 0; i < vector.size(); i++) {    
+  for (int i = 0; i < vector.size(); i++) {
     v(i) = vector[i];
   }
 
@@ -56,12 +56,12 @@ FortranLinalg::DenseVector<Precision> loadCSVColumn(std::string filename) {
 FortranLinalg::DenseVector<Precision> loadCSVColumn(std::string filename, std::string columnName) {
   io::CSVReader<1> in(filename.c_str());
   in.read_header(io::ignore_extra_column, columnName);
-  double value;  
+  double value;
   std::vector<double> vec;
   while (in.read_row(value)) {
     vec.push_back(value);
   }
-    
+
   FortranLinalg::DenseVector<Precision> v(vec.size());
   for (int i = 0; i < vec.size(); i++) {
     v(i) = vec[i];
