706911e06c3f5f092019580e4129f555c27497d4
Jonathan Bronson <jonbronson@gmail.com>
2018-01-22 17:44:45 -0700

    Fix formatting.

--- external/morsesmale/external/kernelstats/lib/FirstOrderKernelRegression.h
+++ external/morsesmale/external/kernelstats/lib/FirstOrderKernelRegression.h
@@ -15,11 +15,12 @@ class FirstOrderKernelRegression{
       
   public:
     FirstOrderKernelRegression(FortranLinalg::DenseMatrix<TPrecision> &data, FortranLinalg::DenseMatrix<TPrecision>
-        &labels, GaussianKernel<TPrecision> &k, int knn):Y(data), X(labels), kernel(k){
-	if(knn > X.N()){ knn = X.N(); }
-	A = FortranLinalg::DenseMatrix<TPrecision>(knn, 1+X.M());
+        &labels, GaussianKernel<TPrecision> &k, int knn):Y(data), X(labels), kernel(k) {
+	    if (knn > X.N()){ 
+        knn = X.N(); 
+      }
+	    A = FortranLinalg::DenseMatrix<TPrecision>(knn, 1+X.M());
      	b = FortranLinalg::DenseMatrix<TPrecision>(knn, Y.M());
-
     };
 
 
@@ -86,41 +87,38 @@ FortranLinalg::Matrix<TPrecision> &J, double *sse=NULL){
     FortranLinalg::DenseMatrix<TPrecision> A;
     FortranLinalg::DenseMatrix<TPrecision> b;
 
-
-
-    FortranLinalg::DenseMatrix<TPrecision> ls(FortranLinalg::DenseVector<TPrecision> &x, TPrecision *sse=NULL){
+    FortranLinalg::DenseMatrix<TPrecision> ls(FortranLinalg::DenseVector<TPrecision> &x, 
+        TPrecision *sse=NULL) {
       FortranLinalg::DenseVector<int> knn(A.M());
       FortranLinalg::DenseVector<TPrecision> knnDist(A.M());
       Distance<TPrecision>::computeKNN(X, x, knn, knnDist, sl2metric);
 
-     TPrecision wsum = 0; 
-     for(unsigned int i=0; i < A.M(); i++){
-       unsigned int nn = knn(i);
-       TPrecision w = kernel.f(knnDist(i));
-       A(i, 0) = w;
-       for(unsigned int j=0; j< X.M(); j++){
-         A(i, j+1) = (X(j, nn)-x(j)) * w;
-       }
+      TPrecision wsum = 0; 
+      for(unsigned int i=0; i < A.M(); i++){
+        unsigned int nn = knn(i);
+        TPrecision w = kernel.f(knnDist(i));
+        A(i, 0) = w;
+        for(unsigned int j=0; j< X.M(); j++){
+          A(i, j+1) = (X(j, nn)-x(j)) * w;
+        }
 
-       for(unsigned int m = 0; m<Y.M(); m++){
-	 b(i, m) = Y(m, nn) *w;
-       }
-       wsum += w*w;
-     }
-    
-     FortranLinalg::DenseMatrix<TPrecision> sol = FortranLinalg::Linalg<TPrecision>::LeastSquares(A, b, sse);
-     if(sse != NULL){ 
-       for(int i=0; i<sol.N(); i++){
-         sse[i] /= wsum;
-       }
-     }
+        for(unsigned int m = 0; m<Y.M(); m++){
+          b(i, m) = Y(m, nn) *w;
+        }
+        wsum += w*w;
+      }
 
-     knn.deallocate();
-     knnDist.deallocate();
-     return sol;
-   };
+      FortranLinalg::DenseMatrix<TPrecision> sol = FortranLinalg::Linalg<TPrecision>::LeastSquares(A, b, sse);
+      if(sse != NULL){ 
+        for(int i=0; i<sol.N(); i++){
+          sse[i] /= wsum;
+        }
+      }
 
-
+      knn.deallocate();
+      knnDist.deallocate();
+      return sol;
+    };
 };
 
 
