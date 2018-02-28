ef105d71a062ab15634bfbc719524d5fd54b2429
Jonathan Bronson <jonbronson@gmail.com>
2018-02-28 16:44:22 -0700

    Add application toolbar

--- client/src/application.js
+++ client/src/application.js
@@ -1,6 +1,8 @@
+import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import React from 'react';
 import PropTypes from 'prop-types';
+import Toolbar from 'material-ui/Toolbar';
 import Workspace from './workspace';
 import { withStyles } from 'material-ui/styles';
 
@@ -8,9 +10,13 @@ const drawerWidth = 240;
 const styles = theme => ({
   root: {
     flexGrow: 1,
+    zIndex: 1,
     overflow: 'hidden',
     position: 'relative',
     display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
   },
   drawerPaper: {
     position: 'relative',
@@ -23,7 +29,8 @@ const styles = theme => ({
     minWidth: 0,
     backgroundColor: '#eef',
     padding: '10px',
-  }
+  },
+  toolbar: theme.mixins.toolbar,
 })
 
 /**
@@ -46,11 +53,20 @@ class Application extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Drawer variant="permanent" 
+        <AppBar position='absolute' className={classes.appBar}>
+          <Toolbar>
+            Menu Here.
+          </Toolbar>
+        </AppBar>
+        <Drawer variant='permanent' 
                 classes={{ paper: classes.drawerPaper }}>
+          { /* Add div to account for menu bar */ }
+          <div className={classes.toolbar} />
           Drawer Content here.
         </Drawer>
         <Workspace className={classes.content}>
+            { /* Add div to account for menu bar */ }
+            <div className={classes.toolbar} />
             Insert Application Here.
         </Workspace>
       </div>
