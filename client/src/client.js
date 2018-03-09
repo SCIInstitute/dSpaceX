c190ac086fe4223446c11e050e030d1f65cfcace
Jon Bronson <jonbronson@gmail.com>
2018-03-08 17:14:09 -0700

    Use underscore prefix to denote private methods.

--- client/src/client.js
+++ client/src/client.js
@@ -2,7 +2,7 @@
 
 /**
  * The Client class encapsulates all client-server communication.
- */ 
+ */
 class Client {
   /**
    * The Client Constructor
@@ -18,64 +18,64 @@ class Client {
     // Unique Message Index
     this.messageIndex = 0;
 
-    this.initializeEventHandling_();
+    this._initializeEventHandling();
   }
 
   /**
    * This method uses the DOM Event Handling framework
    * to provide event handling for the Client class.
    */
-  initializeEventHandling_() {
+  _initializeEventHandling() {
     // DOM EventTarget Object
     let target = document.createTextNode(null);
-    
+
     // Pass EventTarget interface calls to DOM EventTarget object.
     this.addEventListener = target.addEventListener.bind(target);
     this.removeEventListener = target.removeEventListener.bind(target);
     this.dispatchEvent = target.dispatchEvent.bind(target);
   }
 
-  dispatch(eventName) {
+  _dispatch(eventName) {
     let event = document.createEvent('Event');
     event.initEvent(eventName, true, true);
     this.dispatchEvent(event);
   }
 
-  connect(url) {    
+  connect(url) {
     const protocolPrefix = 'ws://';
 
     this.socketBd = new WebSocket(protocolPrefix + url, "data-binary-protocol");
     this.socketBd.binaryType = 'arraybuffer';
-    this.socketBd.onopen     = this.onSocketBdOpen_.bind(this);
-    this.socketBd.onclose    = this.onSocketBdClose_.bind(this);
-    this.socketBd.onmessage  = this.onSocketBdMessage_.bind(this);
-    this.socketBd.onerror    = this.onSocketBdError_.bind(this);
+    this.socketBd.onopen     = this._onSocketBdOpen.bind(this);
+    this.socketBd.onclose    = this._onSocketBdClose.bind(this);
+    this.socketBd.onmessage  = this._onSocketBdMessage.bind(this);
+    this.socketBd.onerror    = this._onSocketBdError.bind(this);
 
     this.socketUt = new WebSocket(protocolPrefix + url, "ui-text-protocol");
-    this.socketUt.onopen    = this.onSocketUtOpen_.bind(this);
-    this.socketUt.onclose   = this.onSocketUtClose_.bind(this);
-    this.socketUt.onmessage = this.onSocketUtMessage_.bind(this);
-    this.socketUt.onerror   = this.onSocketUtError_.bind(this);
+    this.socketUt.onopen    = this._onSocketUtOpen.bind(this);
+    this.socketUt.onclose   = this._onSocketUtClose.bind(this);
+    this.socketUt.onmessage = this._onSocketUtMessage.bind(this);
+    this.socketUt.onerror   = this._onSocketUtError.bind(this);
   }
 
   disconnect() {
     // TODO: Implement this method.
   }
 
-  maybeUpdateState() {
+  _maybeUpdateState() {
     if (this.socketBd.readyState === WebSocket.OPEN &&
         this.socketUt.readyState === WebSocket.OPEN) {
-      this.dispatch('connected');
+      this._dispatch('connected');
     } else {
 
     }
   }
 
-  log(message) {
+  _log(message) {
     console.log('wst: ' + message);
   }
 
-  sendData_(data) {
+  _sendData(data) {
 
   }
 
@@ -104,46 +104,46 @@ class Client {
   }
 
 
-  onSocketUtOpen_(event) {
-    this.log(' UI-text WebSocket Connected!');
+  _onSocketUtOpen(event) {
+    this._log(' UI-text WebSocket Connected!');
     // if (wst.txtInit != undefined) {
     //   wst.socketUt.send(wst.txtInit);
     // }
-    this.maybeUpdateState();
+    this._maybeUpdateState();
   }
 
-  onSocketUtClose_(event) {
-    this.log(' UI-text WebSocket Disconnected!');
+  _onSocketUtClose(event) {
+    this._log(' UI-text WebSocket Disconnected!');
     // wstServerDown();
   }
 
-  onSocketUtMessage_(event) {
+  _onSocketUtMessage(event) {
     //  wst.log(" UI-text WebSocket getMessage: " + evt.data);
     // wstServerTextMessage(evt.data);
   }
 
-  onSocketUtError_(event) {
-    this.log(' UI-text WebSocket Error: ' + event.data);
-    this.dispatch('error');
+  _onSocketUtError(event) {
+    this._log(' UI-text WebSocket Error: ' + event.data);
+    this._dispatch('error');
   }
 
-  onSocketBdOpen_(event) {
-    this.log(' Data-binary WebSocket Connected!');
-    this.maybeUpdateState();
+  _onSocketBdOpen(event) {
+    this._log(' Data-binary WebSocket Connected!');
+    this._maybeUpdateState();
   }
 
-  onSocketBdClose_(event) {
-    this.log(' Data-binary WebSocket Disconnected!');
+  _onSocketBdClose(event) {
+    this._log(' Data-binary WebSocket Disconnected!');
   }
 
-  onSocketBdMessage_(event) {
+  _onSocketBdMessage(event) {
 
   }
-  
-  onSocketBdError_(event) {
+
+  _onSocketBdError(event) {
     // alert(' Not connected to Server: Try reloading the page!');
-    this.log(' Data-binary WebSocket Error: ' + event.data);
-    this.dispatch('error');
+    this._log(' Data-binary WebSocket Error: ' + event.data);
+    this._dispatch('error');
   }
 }
 
