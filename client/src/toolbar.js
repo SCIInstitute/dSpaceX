529a339af025c3a9ce90ccb49666d2f2c8c9331a
Jonathan Bronson <jonbronson@gmail.com>
2018-03-14 11:01:44 -0600

    Fix linting errors for toolbar.js

--- client/src/toolbar.js
+++ client/src/toolbar.js
@@ -4,24 +4,40 @@ import React from 'react';
 import MaterialToolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
+/**
+ * The Toolbar Component presents an application toolbar
+ * for users to have access to global level controls.
+ */
 class Toolbar extends React.Component {
+  /**
+   * Toolbar constuctor.
+   * @param {object} props
+   */
   constructor(props) {
     super(props);
   }
 
+  /**
+   * Renders the component to HTML.
+   * @return {HTML}
+   */
   render() {
     return (
       <AppBar position='absolute' className={this.props.className}>
         <MaterialToolbar>
-          <Typography variant="title" color="inherit">
+          <Typography variant='title' color='inherit'>
             dSpaceX
           </Typography>
-          <div style={{ width: '100%', display: 'flex', flexDirection: 'row-reverse' }}>
-            <Button variant="raised" style={{ backgroundColor: 'white' }}
+          <div style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'row-reverse',
+          }}>
+            <Button variant='raised' style={{ backgroundColor:'white' }}
                     onClick={this.props.onConnectClick}
                     disabled={this.props.connectedToServer} >
               Connect
-            </Button>            
+            </Button>
           </div>
         </MaterialToolbar>
       </AppBar>
