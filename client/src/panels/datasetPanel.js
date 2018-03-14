43cb56bedd71bb608dc06fb21e2183869c5e7d13
Jonathan Bronson <jonbronson@gmail.com>
2018-03-14 10:57:49 -0600

    Fix linting errors for datasetPanel.js

--- client/src/panels/datasetPanel.js
+++ client/src/panels/datasetPanel.js
@@ -1,5 +1,5 @@
-import { FormControl, FormHelperText } from 'material-ui/Form';
-import Input, { InputLabel } from 'material-ui/Input';
+import { FormControl } from 'material-ui/Form';
+import { InputLabel } from 'material-ui/Input';
 import { MenuItem } from 'material-ui/Menu';
 import React from 'react';
 import Paper from 'material-ui/Paper';
@@ -8,49 +8,62 @@ import Select from 'material-ui/Select';
 import { withStyles } from 'material-ui/styles';
 
 
-// TODO: This list should be populated from the server.
-const datasetList = [ 'Colorado', 'Sandia' ];
-
+/**
+ * The DatasetPanel component allows the user to control
+ * the active dataset and see dataset metadata.
+ */
 class DatasetPanel extends React.Component {
+  /**
+   * The DatasetPanel constructor.
+   * @param {object} props
+   */
   constructor(props) {
     super(props);
     this.state = {
-      dataset: ''
+      dataset: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
-  }  
+  }
 
+  /**
+   * Handles the user changing the current active dataset.
+   * @param {Event} event
+   */
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]:event.target.value });
   };
 
+  /**
+   * Renders the component to HTML.
+   * @return {HTML}
+   */
   render() {
     const { classes } = this.props;
     return (
-      <Paper style={{padding: '15px', paddingBottom: '50px'}}>
-        <div style={{display: 'flex',  flexDirection:'column'}}>
+      <Paper style={{ padding:'15px', paddingBottom:'50px' }}>
+        <div style={{ display:'flex', flexDirection:'column' }}>
           <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="dataset-field">Dataset</InputLabel>
-            <Select value={this.state.dataset}
-                onChange={this.handleChange}
-                inputProps={{
-                  name: 'dataset',
-                  id: 'dataset-field',
-                }}
-              >
+            <InputLabel htmlFor='dataset-field'>Dataset</InputLabel>
+            <Select value={this.state.dataset} onChange={this.handleChange}
+              inputProps={{
+                name: 'dataset',
+                id: 'dataset-field',
+              }}>
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
               {
-                this.props.datasets.map(dataset => (
-                  <MenuItem value={dataset.name} key={dataset.id}>{dataset.name}</MenuItem>  
+                this.props.datasets.map((dataset) => (
+                  <MenuItem value={dataset.name} key={dataset.id}>
+                    {dataset.name}
+                  </MenuItem>
                 ))
               }
-            </Select>            
-          </FormControl>              
+            </Select>
+          </FormControl>
         </div>
-      </Paper>        
+      </Paper>
     );
   }
 }
