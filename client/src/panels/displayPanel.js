bca2b1809a6c92627023eccd6022a5d19e788727
Jonathan Bronson <jonbronson@gmail.com>
2018-03-14 10:59:13 -0600

    Fix linting errors for displayPanel.js

--- client/src/panels/displayPanel.js
+++ client/src/panels/displayPanel.js
@@ -5,16 +5,27 @@ import { ExpansionPanelDetails } from 'material-ui/ExpansionPanel';
 import React from 'react';
 import Typography from 'material-ui/Typography';
 
+/**
+ * The DisplayPanel component provides a user a means to adjust
+ * various global view settings from the side drawer.
+ */
 class DisplayPanel extends React.Component {
+  /**
+   * DisplayPanel constructor.
+   * @param {object} props
+   */
   constructor(props) {
     super(props);
   }
 
+  /**
+   * Renders the component to HTML.
+   * @return {HTML}
+   */
   render() {
-    const { classes } = this.props;
     return (
       // TODO: set disabled only when there's no case data.
-      <ExpansionPanel style={{margin: '1px'}} disabled={true}>
+      <ExpansionPanel style={{ margin:'1px' }} disabled={true}>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
           <Typography>Display</Typography>
         </ExpansionPanelSummary>
