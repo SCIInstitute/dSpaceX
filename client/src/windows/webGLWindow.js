ada83833fcb2c039bc82f47f1c131c8908bc2a32
Jonathan Bronson <jonbronson@gmail.com>
2018-03-14 11:08:05 -0600

    Fix linting errors for webGLWindow.js

--- client/src/windows/webGLWindow.js
+++ client/src/windows/webGLWindow.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-
+/**
+ * A base WebGL Window Component.
+ */
 class WebGLWindow extends React.Component {
+  /**
+   * WebGLWindow constructor.
+   * @param {object} props
+   */
   constructor(props) {
     super(props);
 
     this.gl = null;
     this.canvas = null;
     this.vertices = null;
+    this.vertex_array = null;
     this.vertex_buffer = null;
     this.shaderProgram = null;
   }
 
+  /**
+   * Initial setup for the webgl canvas.
+   */
   initGL() {
     const canvas = this.refs.canvas;
     let gl = canvas.getContext('webgl');
@@ -23,8 +33,12 @@ class WebGLWindow extends React.Component {
     this.createBuffers(gl);
   }
 
-  createShaders(gl) {    
-    const vertexShaderSource = 
+  /**
+   * Compiles vertex and fragment shader programs.
+   * @param {object} gl The OpenGL context.
+   */
+  createShaders(gl) {
+    const vertexShaderSource =
       'attribute vec2 coordinates;                 ' +
       'void main(void) {                           ' +
       '  gl_Position = vec4(coordinates, 0.0, 1.0);' +
@@ -33,55 +47,74 @@ class WebGLWindow extends React.Component {
     gl.shaderSource(vertexShader, vertexShaderSource);
     gl.compileShader(vertexShader);
 
-    let fragmentShaderSource = 
+    let fragmentShaderSource =
       'void main(void) {                           ' +
       '  gl_FragColor = vec4(0.0, 0.5, 0.0, 1.0);  ' +
       '}                                           ';
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderSource);
-    gl.compileShader(fragmentShader); 
+    gl.compileShader(fragmentShader);
 
     let shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);   
+    gl.useProgram(shaderProgram);
 
     this.shaderProgram = shaderProgram;
   }
 
+  /**
+   * Creates vertex buffers for dummy data.
+   * @param {object} gl The OpenGL context.
+   */
   createBuffers(gl) {
     this.vertices = [0, 0.5, -0.5, -0.5, 0.5, -0.5];
     this.vertex_buffer = gl.createBuffer();
+    this.vertex_array = new Float32Array(this.vertices);
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null); 
+    gl.bufferData(gl.ARRAY_BUFFER, this.vertex_array, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
   }
 
+  /**
+   * Resize the canvas renderer to match the dom size.
+   * @param {object} gl The OpenGL context.
+   */
   resizeCanvas() {
     let canvas = this.refs.canvas;
-    canvas.width  = canvas.clientWidth;
+    canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
   }
 
+  /**
+   * Callback invoked before the component receives new props.
+   */
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeCanvas.bind(this));
+  }
+
+  /**
+   * Callback invoked immediately after the component is mounted.
+   */
   componentDidMount() {
-    this.initGL();     
+    this.initGL();
     this.resizeCanvas();
-    window.addEventListener("resize", this.resizeCanvas.bind(this)); 
+    window.addEventListener('resize', this.resizeCanvas.bind(this));
     requestAnimationFrame(this.drawScene.bind(this));
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.resizeCanvas.bind(this));
-  }
-
+  /**
+   * Renders the OpenGL Content to the canvas.
+   */
   drawScene() {
     const canvas = this.refs.canvas;
     let gl = canvas.getContext('webgl');
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
 
-    let coordinateAttrib = gl.getAttribLocation(this.shaderProgram, 'coordinates');
+    let coordinateAttrib =
+        gl.getAttribLocation(this.shaderProgram, 'coordinates');
     gl.vertexAttribPointer(coordinateAttrib, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coordinateAttrib);
     gl.enable(gl.DEPTH_TEST);
@@ -91,10 +124,17 @@ class WebGLWindow extends React.Component {
     gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
 
+  /**
+   * Renders the component to HTML.
+   * @return {HTML}
+   */
   render() {
     return (
-      <canvas ref='canvas' className='glCanvas'
-              style={{width: '100%', height: '100%', border: '1px dashed gray'}}>
+      <canvas ref='canvas' className='glCanvas' style={{
+        width: '100%',
+        height: '100%',
+        border: '1px dashed gray',
+      }}>
       </canvas>
     );
   }
