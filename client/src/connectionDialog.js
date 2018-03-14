6de328dfe78b179444879e34e88806ac3fd01166
Jonathan Bronson <jonbronson@gmail.com>
2018-03-14 10:51:13 -0600

    Fix linting errors for connectionDialog.js

--- client/src/connectionDialog.js
+++ client/src/connectionDialog.js
@@ -5,18 +5,20 @@ import { DialogActions } from 'material-ui/Dialog';
 import { DialogContent } from 'material-ui/Dialog';
 import { DialogContentText } from 'material-ui/Dialog';
 import { DialogTitle } from 'material-ui/Dialog';
-import TextField from "material-ui/TextField";
+import TextField from 'material-ui/TextField';
 import React from 'react';
 
-const ConnectionState = {
-  DISCONNECTED: 0,
-  CONNECTING: 1,
-  CONNECTED: 2,
-};
-
+/**
+ * The Dialog shown when a user attempts to connect to the server.
+ * Provides a method for user to change the host address.
+ */
 class ConnectionDialog extends React.Component {
+  /**
+   * ConnectDialog constructor.
+   * @param {object} props
+   */
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       open: false,
@@ -31,54 +33,79 @@ class ConnectionDialog extends React.Component {
     this.handleHostChange = this.handleHostChange.bind(this);
     this.onConnect = this.onConnect.bind(this);
     this.onError = this.onError.bind(this);
-
-    this.client = this.props.client;    
+    this.client = this.props.client;
   }
 
+  /**
+   * Opens the dialog window.
+   */
   open() {
-    this.setState({ open: true });
+    this.setState({ open:true });
   }
 
+  /**
+   * Closes the dialog window.
+   */
   close() {
-    this.setState({ open: false });
+    this.setState({ open:false });
   }
 
+  /**
+   * Initiates connection to the host server.
+   */
   connect() {
     // debounce multiple connection attempts
     if (this.state.isConnecting) {
       return;
     }
 
-    this.setState({ isConnecting: true });
+    this.setState({ isConnecting:true });
     this.client.addEventListener('connected', this.onConnect);
     this.client.addEventListener('error', this.onError);
-    this.client.connect(this.state.host);    
+    this.client.connect(this.state.host);
   }
 
-  cancel() {    
+  /**
+   * Cancels the attempted server connection.
+   */
+  cancel() {
     this.client.disconnect();
-    this.setState({ isConnecting: false, open: false});
+    this.setState({ isConnecting:false, open:false });
   }
 
+  /**
+   * Responds to connection event to reset and close the dialog.
+   */
   onConnect() {
-    this.setState({ isConnecting: false });
+    this.setState({ isConnecting:false });
     this.client.removeEventListener('connected', this.onConnect);
     this.client.removeEventListener('error', this.onError);
     this.close();
   }
 
+  /**
+   * Responds to a failed connect event.
+   */
   onError() {
     this.client.removeEventListener('connected', this.onConnect);
     this.client.removeEventListener('error', this.onError);
-    this.setState({ isConnecting: false });
+    this.setState({ isConnecting:false });
   }
 
+  /**
+   * Handles the user updating the host address.
+   * @param {Event} event
+   */
   handleHostChange(event) {
     this.setState({
       host: event.target.value,
     });
   }
 
+  /**
+   * Renders the component to HTML.
+   * @return {HTML}
+   */
   render() {
     return (
       <Dialog
@@ -87,23 +114,23 @@ class ConnectionDialog extends React.Component {
           aria-labelledby="form-dialog-title"
         >
          {
-           this.state.isConnecting ? 
+           this.state.isConnecting ?
            <div style={{
               padding: 0,
               margin: 0,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
-              justifyContent: 'center'
+              justifyContent: 'center',
             }}>
             <DialogTitle id="form-dialog-title">Connecting...</DialogTitle>
             <CircularProgress/>
            </div> :
            <DialogTitle id="form-dialog-title">Connect to Server</DialogTitle>
          }
-          <DialogContent>          
+          <DialogContent>
             {
-              this.state.isConnecting ? ''  :
+              this.state.isConnecting ? '' :
                 <DialogContentText>
                   Please enter the [host]:[port] of the server.
                 </DialogContentText>
@@ -125,7 +152,8 @@ class ConnectionDialog extends React.Component {
             <Button onClick={this.cancel} color="primary">
               Cancel
             </Button>
-            <Button onClick={this.connect} color="primary" disabled={this.state.isConnecting}>
+            <Button onClick={this.connect} color="primary"
+                    disabled={this.state.isConnecting}>
               Connect
             </Button>
           </DialogActions>
