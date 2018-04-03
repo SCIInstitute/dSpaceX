478bdb180e1556157d29ca26a0f6487e9749931b
Jon Bronson <jonbronson@gmail.com>
2018-04-03 16:55:12 -0600

    Build with tclap in lib folder if not present on machine.

--- cli/HDVizProcess.cpp
+++ cli/HDVizProcess.cpp
@@ -1,6 +1,6 @@
 #include "HDProcessor.h"
 #include "HDProcessResultSerializer.h"
-#include <tclap/CmdLine.h>
+#include "tclap/CmdLine.h"
 #include "Precision.h"
 #include "Linalg.h"
 #include "LinalgIO.h"
@@ -17,17 +17,17 @@ void writeData(HDProcessResult *result, std::string path);
 int main(int argc, char **argv){
   TCLAP::CmdLine cmd("Compute MS-Complex and summary representation", ' ', "1");
   TCLAP::ValueArg<Precision> sigmaArg("s" /* flag */, "sigma" /* name */,
-      "Kernel regression bandwith (sigma for Gaussian)" /* description */, 
+      "Kernel regression bandwith (sigma for Gaussian)" /* description */,
       true /* required */, 1 /* default */, "float" /* type */);
-  cmd.add(sigmaArg);  
-    
-  TCLAP::ValueArg<std::string> xArg("x" /* flag */, "domain" /* name */, 
-      "Filename for Data points in domain" /* description */, 
+  cmd.add(sigmaArg);
+
+  TCLAP::ValueArg<std::string> xArg("x" /* flag */, "domain" /* name */,
+      "Filename for Data points in domain" /* description */,
       true /* required */, "", "string");
   cmd.add(xArg);
 
-  TCLAP::ValueArg<std::string> fArg("f" /* flag */, "function" /* name */, 
-      "f(x), Filename for function value for each data point in X" /* description */, 
+  TCLAP::ValueArg<std::string> fArg("f" /* flag */, "function" /* name */,
+      "f(x), Filename for function value for each data point in X" /* description */,
       true /* required */, "", "string");
   cmd.add(fArg);
 
@@ -37,36 +37,36 @@ int main(int argc, char **argv){
   cmd.add(outArg);
 
   TCLAP::ValueArg<int> pArg("p" /* flag */, "persistence",
-      "Number of persistence levels to compute; all = -1 , default = 20" /* description */, 
+      "Number of persistence levels to compute; all = -1 , default = 20" /* description */,
       true /* required */, 20 /* default */, "integer");
-  cmd.add(pArg);  
-  
+  cmd.add(pArg);
+
   TCLAP::ValueArg<int> samplesArg("n" /* flag */, "samples" /* name */,
-      "Number of samples for each regression curve, default = 50" /* description */, 
+      "Number of samples for each regression curve, default = 50" /* description */,
       true /* required */, 50 /* default */,  "integer" /* type */);
-  cmd.add(samplesArg);  
+  cmd.add(samplesArg);
 
   TCLAP::ValueArg<int> knnArg("k" /* flag */, "knn" /* name */,
-      "Number of nearest neighbors for Morse-Smale approximation, default = 50" /* description */, 
+      "Number of nearest neighbors for Morse-Smale approximation, default = 50" /* description */,
       true /* required */, 50,  "integer" /* type */);
-  cmd.add(knnArg); 
+  cmd.add(knnArg);
 
-  TCLAP::SwitchArg randArg("r" /* flag */, "random" /* name */, 
-      "Adds 0.0001 * range(f) uniform random noise to f, " 
-      "in case of 0 gradients due to equivivalent values" /* description */, 
-      false /* required */); 
+  TCLAP::SwitchArg randArg("r" /* flag */, "random" /* name */,
+      "Adds 0.0001 * range(f) uniform random noise to f, "
+      "in case of 0 gradients due to equivivalent values" /* description */,
+      false /* required */);
   cmd.add(randArg);
 
-  TCLAP::ValueArg<double> smoothArg("" /* flag */, "smooth" /* name */, 
-      "Smooth function values to nearest nieghbor averages" /* description */, 
-      false /* required */, 0 /* default */, "double" /* type */); 
+  TCLAP::ValueArg<double> smoothArg("" /* flag */, "smooth" /* name */,
+      "Smooth function values to nearest nieghbor averages" /* description */,
+      false /* required */, 0 /* default */, "double" /* type */);
   cmd.add(smoothArg);
 
-    
+
   try {
-    cmd.parse( argc, argv );    
-  } catch (TCLAP::ArgException &e){ 
-    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; 
+    cmd.parse( argc, argv );
+  } catch (TCLAP::ArgException &e){
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
     return -1;
   }
 
@@ -74,16 +74,16 @@ int main(int argc, char **argv){
   // TODO: Move into a data loading library
   DenseMatrix<Precision> x = LinalgIO<Precision>::readMatrix(xArg.getValue());
   DenseVector<Precision> y = LinalgIO<Precision>::readVector(fArg.getValue());
-      
+
   HDProcessResult *result = nullptr;
   try {
     HDProcessor processor;
     result = processor.process(
         x /* domain */,
         y /* function */,
-        knnArg.getValue() /* knn */,        
+        knnArg.getValue() /* knn */,
         samplesArg.getValue() /* samples */,
-        pArg.getValue() /* persistence */,        
+        pArg.getValue() /* persistence */,
         randArg.getValue() /* random */,
         sigmaArg.getValue() /* sigma */,
         smoothArg.getValue() /* smooth */);
@@ -94,11 +94,11 @@ int main(int argc, char **argv){
   // Check result and maybe write to disk.
   if (result != nullptr) {
     std::cout << "Successfully returned process result. Saving..." << std::endl;
-    HDProcessResultSerializer::write(result, outArg.getValue());    
+    HDProcessResultSerializer::write(result, outArg.getValue());
     std::cout << "Done." << std::endl;
   } else {
     std::cout << "Something went wrong. Process returned null result." << std::endl;
   }
-  
+
   return 0;
 }
