67db33b67100793e675c40c3657aa3900ac268a2
Jon Bronson <jonbronson@gmail.com>
2018-04-04 21:22:28 -0600

    Update styling to improve readability

--- lib/dimred/MetricTensorKNNNeighborhood.h
+++ lib/dimred/MetricTensorKNNNeighborhood.h
@@ -13,62 +13,57 @@
 #include <vector>
 
 template <typename TPrecision>
-class MetricTensorKNNNeighborhood : public Neighborhood<TPrecision>{
- 
-                                          
+class MetricTensorKNNNeighborhood : public Neighborhood<TPrecision> {
   public:
-    
-    MetricTensorKNNNeighborhood(int k, TPrecision a, TPrecision value) : 
-                               knn(k), alpha(a), val(value){};
+    MetricTensorKNNNeighborhood(int k, TPrecision a, TPrecision value)
+        : knn(k), alpha(a), val(value) {};
 
-   
-
-    FortranLinalg::SparseMatrix<TPrecision> generateNeighborhood(FortranLinalg::Matrix<TPrecision> &data){
+    FortranLinalg::SparseMatrix<TPrecision> generateNeighborhood(
+        FortranLinalg::Matrix<TPrecision> &data) {
       using namespace FortranLinalg;
-      
-      //knn 
+
+      // knn
       DenseMatrix<int> knns(knn, data.N());
       DenseMatrix<TPrecision> knnDists(knn, data.N());
-      Distance<TPrecision>::computeKNN(data, knns, knnDists, euclideanMetric);  
+      Distance<TPrecision>::computeKNN(data, knns, knnDists, euclideanMetric);
 
-      //complete adjancy matrix
+      // complete adjancy matrix
       SparseMatrix<TPrecision> adj(data.N(), data.N(), val);
 
-      //Metric tensors J = G^T*G
-      //save G fro creating mahalanobis distance metric afterwards
+      // Metric tensors J = G^T*G
+      // save G fro creating mahalanobis distance metric afterwards
       std::vector< DenseMatrix<TPrecision> > J(data.N());
 
-      //Compute Metric Tensor at each point
+      // Compute Metric Tensor at each point
       DenseMatrix<TPrecision> ndata(data.M(), knn);
 
-      for(unsigned int i=0; i< adj.N(); i++){
+      for (unsigned int i = 0; i < adj.N(); i++) {
         std::cout << "Computing G_" << i << std::endl;
-        //Fill neighborhood matrix 
+        // Fill neighborhood matrix
         int index = 0;
         for(int k=0; k<knn; ++k){
           for(unsigned int j=0; j < data.M(); j++){
             ndata(j, index) = data(j, knns(k, i));
-          }          
+          }
           index++;
         }
 
 
-        //Do local pca and compute G
+        // Do local pca and compute G
         PCA<TPrecision> pca(ndata, 0, true);
         for(unsigned int j=0; j<pca.ev.N(); j++){
           for(unsigned int k=0; k<pca.ev.M(); k++){
             pca.ev(k, j) /= sqrt(pca.ew(j));
           }
         }
-        J[i] = Linalg<Precision>::Multiply(pca.ev, pca.ev, false, true); 
+        J[i] = Linalg<Precision>::Multiply(pca.ev, pca.ev, false, true);
 
-        
+
 
         //Write tensor for visualization in matlab
         //std::stringstream ss;
         //ss << "tensor_" << i <<".data";
         //LinalgIO<TPrecision>::writeMatrix(ss.str().c_str(), J[i]);
-
         pca.cleanup();
       }
 
@@ -79,35 +74,32 @@ class MetricTensorKNNNeighborhood : public Neighborhood<TPrecision>{
       DenseVector<TPrecision> nnDists(knn);
 
       DenseMatrix<TPrecision> JTmp(J[0].M(), J[0].N());
-        for(unsigned int i=0; i< adj.N(); i++){
-          std::cout << "Smoothing neighborhood and computing knn for " << i << std::endl;
+      for (unsigned int i = 0; i < adj.N(); i++) {
+        std::cout << "Smoothing neighborhood and computing knn for " << i << std::endl;
 
-
-          //GTmp_i = 1 / (1+alpha) ( G_i + alpha / knn * sum_nn G_nn)
-          Linalg<TPrecision>::Copy(J[i], JTmp);
-          //Compute smoothed metric tensor
-          for(int k=1; k<knn; ++k){
-            Linalg<TPrecision>::Add(JTmp, J[knns(k, i)], JTmp);
-          }
-          Linalg<TPrecision>::Scale(JTmp, alpha/knn, JTmp);
-          Linalg<TPrecision>::Add(JTmp, J[i], JTmp);
-          Linalg<TPrecision>::Scale(JTmp, 1/(1+alpha), JTmp);
-          
-          //Write updated tensor for visualization in matlab
-          //std::stringstream ss;
-          //ss << "tensorsmooth_" << i <<".data";
-          //LinalgIO<TPrecision>::writeMatrix(ss.str().c_str(), JTmp);
-          
-          //Compute new weighted adjacencies
-          MahalanobisMetric2<TPrecision> mahal(JTmp);
-          Distance<TPrecision>::computeKNN(data, i, nn, nnDists, mahal);
-          typename SparseMatrix<TPrecision>::SparseEntry *entry = adj.getEntries(i); 
-          for(int k=1; k < knn; k++){
-            entry->operator[](nn(k)) = nnDists(k);
-          }
+        //GTmp_i = 1 / (1+alpha) ( G_i + alpha / knn * sum_nn G_nn)
+        Linalg<TPrecision>::Copy(J[i], JTmp);
+        //Compute smoothed metric tensor
+        for(int k=1; k<knn; ++k){
+          Linalg<TPrecision>::Add(JTmp, J[knns(k, i)], JTmp);
         }
+        Linalg<TPrecision>::Scale(JTmp, alpha/knn, JTmp);
+        Linalg<TPrecision>::Add(JTmp, J[i], JTmp);
+        Linalg<TPrecision>::Scale(JTmp, 1/(1+alpha), JTmp);
 
+        //Write updated tensor for visualization in matlab
+        //std::stringstream ss;
+        //ss << "tensorsmooth_" << i <<".data";
+        //LinalgIO<TPrecision>::writeMatrix(ss.str().c_str(), JTmp);
 
+        //Compute new weighted adjacencies
+        MahalanobisMetric2<TPrecision> mahal(JTmp);
+        Distance<TPrecision>::computeKNN(data, i, nn, nnDists, mahal);
+        typename SparseMatrix<TPrecision>::SparseEntry *entry = adj.getEntries(i);
+        for (int k = 1; k < knn; k++) {
+          entry->operator[](nn(k)) = nnDists(k);
+        }
+      }
 
 
       //cleanup memmory
@@ -121,21 +113,14 @@ class MetricTensorKNNNeighborhood : public Neighborhood<TPrecision>{
         J[i].deallocate();
       }
 
-
-
-      return adj; 
-    
+      return adj;
     };
 
-  
   private:
     EuclideanMetric<Precision> euclideanMetric;
     int knn;
     TPrecision alpha;
     TPrecision val;
-
-
-                                         
 };
 
 #endif
