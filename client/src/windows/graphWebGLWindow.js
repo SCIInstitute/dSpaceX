3b5b3ad2f2e6e4e90b2fd39fe1548542a2250538
Jim <Jimmerrell@hotmail.com>
2018-04-12 16:50:15 -0600

    1) Added webGLErrorCheck
    2) Added edge class
    3) PoC for edge class implementation

--- client/src/windows/graphWebGLWindow.js
+++ client/src/windows/graphWebGLWindow.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+
+/**
+ * WebGL error check wrapper - logs to console
+ * @param {any} gl
+ */
+let webGLErrorCheck = function(gl) {
+  let error = gl.getError();
+  if (error != gl.NO_ERROR) {
+    const e = new Error();
+    const regex = /\((.*):(\d+):(\d+)\)$/;
+    const match = regex.exec(e.stack.split('\n')[2]);
+
+    let str = 'GL Error @ line:' + match[2] +
+      'in file:' + match[1] + ' Error#: ' + error;
+    console.log(str);
+  }
+};
+
+
 /**
    * Creates a basic quad with 4 vertices and 6 indices.
    */
@@ -19,9 +38,9 @@ class Quad {
     this.height = height;
 
     let minX = -(width / 2.0) + centerX;
-    let maxX = minX + (width / 2.0);
+    let maxX = minX + width;
     let minY = -(height / 2.0) + centerY;
-    let maxY = minY + (height / 2.0);
+    let maxY = minY + height;
 
     this.vertices = [
       minX, maxY,
@@ -33,6 +52,25 @@ class Quad {
     this.indices = [
       n, n + 1, n + 2,
       n + 2, n + 1, n + 3];
+  }
+}
+
+/**
+   * Creates a basic edge from 2 points
+   */
+class Edge {
+  /**
+   * Edge constructor
+   * @param {object} x1
+   * @param {object} y1
+   * @param {object} x2
+   * @param {object} y2
+   */
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
   }
 }
 
@@ -55,6 +93,11 @@ class GraphWebGLWindow extends React.Component {
     this.shaderProgram = null;
     this.vertices = null;
     this.indices = null;
+    this.nodes = null;
+    this.edges = null;
+    this.edgeVerts = null;
+    this.edgeVerts_array = null;
+    this.edgeVerts_buffer = null;
   }
 
   /**
@@ -71,21 +114,26 @@ class GraphWebGLWindow extends React.Component {
 
     this.createShaders(gl);
     this.createBuffers(gl);
+
+    webGLErrorCheck(gl);
   }
 
   /**
    * Creates the geometry to be rendered.
    */
   createGeometry() {
-    let width = .1;
-    let height = .1;
+    let width = .2;
+    let height = .2;
 
     this.vertices = [];
     this.indices = [];
+    this.edgeVerts = [];
+    this.edges = [];
+    this.nodes = [];
 
-    for (let y = 0; y < 4; y++) {
-      for (let x = 0; x < 4; x++) {
-        let pX = -.75 + (x * (width*2));
+    for (let y = 0; y < 5; y++) {
+      for (let x = 0; x < 5; x++) {
+        let pX = -.75 + (x * (width * 2));
         let pY = .75 - (y * (height * 2));
         let firstIndex = 0;
         if (this.indices.length >= 6) {
@@ -96,7 +144,20 @@ class GraphWebGLWindow extends React.Component {
 
         this.vertices = this.vertices.concat(quad.vertices);
         this.indices = this.indices.concat(quad.indices);
+
+        this.nodes.push(quad);
       }
+    }
+
+    // build the edges
+    let i = 0;
+    let j = 1;
+    for (; j < this.nodes.length; i++, j++) {
+      let edge = new Edge(this.nodes[i].X, this.nodes[i].Y,
+        this.nodes[j].X, this.nodes[j].Y);
+
+      this.edges.push(edge);
+      this.edgeVerts.push(edge.x1, edge.y1, edge.x2, edge.y2);
     }
   }
 
@@ -106,21 +167,25 @@ class GraphWebGLWindow extends React.Component {
    */
   createShaders(gl) {
     const vertexShaderSource =
-            'attribute vec2 coordinates;                 ' +
-            'void main(void) {                           ' +
-            '  gl_Position = vec4(coordinates, 0.0, 1.0);' +
-            '}                                           ';
+      'attribute vec2 coordinates;                 ' +
+      'void main(void) {                           ' +
+      '  gl_Position = vec4(coordinates, 0.0, 1.0);' +
+      '}                                           ';
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderSource);
     gl.compileShader(vertexShader);
 
+    webGLErrorCheck(gl);
+
     let fragmentShaderSource =
-            'void main(void) {                           ' +
-            '  gl_FragColor = vec4(0.8, 0.2, 0.2, 1.0);  ' +
-            '}                                           ';
+      'void main(void) {                           ' +
+      '  gl_FragColor = vec4(0.8, 0.2, 0.2, 1.0);  ' +
+      '}                                           ';
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderSource);
     gl.compileShader(fragmentShader);
+
+    webGLErrorCheck(gl);
 
     let shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
@@ -129,6 +194,8 @@ class GraphWebGLWindow extends React.Component {
     gl.useProgram(shaderProgram);
 
     this.shaderProgram = shaderProgram;
+
+    webGLErrorCheck(gl);
   }
 
 
@@ -137,11 +204,23 @@ class GraphWebGLWindow extends React.Component {
    * @param {object} gl The OpenGL context.
    */
   createBuffers(gl) {
+    // Quad buffers
     this.vertex_buffer = gl.createBuffer();
     this.vertex_array = new Float32Array(this.vertices);
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
     gl.bufferData(gl.ARRAY_BUFFER, this.vertex_array, gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    webGLErrorCheck(gl);
+
+    // Edge buffers
+    this.edgeVerts_buffer = gl.createBuffer();
+    this.edgeVerts_array = new Float32Array(this.edgeVerts);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.edgeVerts_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this.edgeVerts_array, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    webGLErrorCheck(gl);
   }
 
   /**
@@ -186,6 +265,8 @@ class GraphWebGLWindow extends React.Component {
     const canvas = this.refs.canvas;
     let gl = canvas.getContext('webgl');
 
+    webGLErrorCheck(gl);
+
     // TODO: pull out into method
     let indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
@@ -195,17 +276,30 @@ class GraphWebGLWindow extends React.Component {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
 
     let coordinateAttrib =
-            gl.getAttribLocation(this.shaderProgram, 'coordinates');
+      gl.getAttribLocation(this.shaderProgram, 'coordinates');
     gl.vertexAttribPointer(coordinateAttrib, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coordinateAttrib);
-    gl.enable(gl.DEPTH_TEST);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0, 0, canvas.width, canvas.height);
 
-    // gl.drawArrays(gl.TRIANGLES, 0, 6);
+
     gl.drawElements(gl.TRIANGLES, this.indices.length,
       gl.UNSIGNED_SHORT, indexBuffer);
+
+    webGLErrorCheck(gl);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.edgeVerts_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this.edgeVerts_array, gl.STATIC_DRAW);
+    coordinateAttrib = gl.getAttribLocation(this.shaderProgram, 'coordinates');
+    gl.vertexAttribPointer(coordinateAttrib, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coordinateAttrib);
+    gl.drawArrays(gl.LINES, 0, this.edgeVerts.length / 2);
+
+    webGLErrorCheck(gl);
   }
 
   /**
