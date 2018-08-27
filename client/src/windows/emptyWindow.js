65463ffadc19ff73530eb5f3fe65061e3a7a98d9
Jonathan Bronson <jonbronson@gmail.com>
2018-08-27 12:07:14 -0600

    Add empty window component for placeholders.

---
+++ client/src/windows/emptyWindow.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+/**
+ * A Window Component that renders nothing.
+ */
+class EmptyWindow extends React.Component {
+  /**
+   * EmptyWindow constructor.
+   * @param {object} props
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * Renders the component to HTML.
+   * @return {HTML}
+   */
+  render() {
+    return (
+      <div style={{ 
+        borderColor: '#ddddff', 
+        borderWidth: '2px', 
+        borderStyle: 'dashed',
+      }}/>
+    );
+  }
+}
+
+export default EmptyWindow;
