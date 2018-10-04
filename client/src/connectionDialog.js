1a81ca696ba2c306dcf2df5fd9056122124d8bef
Jonathan Bronson <jonbronson@gmail.com>
2018-10-04 09:49:26 -0600

    Convert connectionDialog to material-ui stable

--- client/src/connectionDialog.js
+++ client/src/connectionDialog.js
@@ -1,12 +1,12 @@
-import Button from 'material-ui/Button';
-import { CircularProgress } from 'material-ui/Progress';
-import Dialog from 'material-ui/Dialog';
-import { DialogActions } from 'material-ui/Dialog';
-import { DialogContent } from 'material-ui/Dialog';
-import { DialogContentText } from 'material-ui/Dialog';
-import { DialogTitle } from 'material-ui/Dialog';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import React from 'react';
-import TextField from 'material-ui/TextField';
+import TextField from '@material-ui/core/TextField';
 
 
 /**
