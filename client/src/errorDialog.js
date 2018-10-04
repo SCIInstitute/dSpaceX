e746714a7ee607ddce0831c9775125e4eff93671
Jonathan Bronson <jonbronson@gmail.com>
2018-10-04 09:51:07 -0600

    Convert errorDialog to use material-ui stable.

--- client/src/errorDialog.js
+++ client/src/errorDialog.js
@@ -1,9 +1,9 @@
-import Button from 'material-ui/Button';
-import Dialog from 'material-ui/Dialog';
-import { DialogActions } from 'material-ui/Dialog';
-import { DialogContent } from 'material-ui/Dialog';
-import { DialogContentText } from 'material-ui/Dialog';
-import { DialogTitle } from 'material-ui/Dialog';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import React from 'react';
 
 /**
