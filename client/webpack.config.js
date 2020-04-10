a296fcb72bb7826c74ca08cf353bf0860d91ff16
Cameron Christensen <cam@sci.utah.edu>
2020-04-10 17:37:43 -0600

    fix npm install/update compile errors; update babel; add watch scripts to packages.json to enable automatic rebuild of bundle (must still run web server: I used 'npm install --save-dev http-server', then 'http-server . -p 8080' from the client directory in a separate terminal, and 'npm run watch' at the same time)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ let config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['babel-preset-env'],
+            presets: ['@babel/preset-env'],
           },
         },
       },
