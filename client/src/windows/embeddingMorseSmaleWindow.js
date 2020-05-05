62e9dffc796aecb0926c4726582453e35c1dffd0
Kyli McKay-Bishop <kyli.nmb@gmail.com>
2020-05-05 15:45:55 -0600

    Drawer apperance is improving but it still adjusts the dimension of the embedding and ms window when it loads, plus the resize of the whole window does not work quite right. No matter if you are shrinking or growing the window the canvas for those views continues to grow

--- client/src/windows/embeddingMorseSmaleWindow.js
+++ client/src/windows/embeddingMorseSmaleWindow.js
@@ -1,35 +1,46 @@
 import EmbeddingWindow from './embeddingWindow';
+import {Grid, GridList} from '@material-ui/core';
 import MorseSmaleWindow from './morseSmaleWindow';
+import Paper from '@material-ui/core/Paper';
 import React from 'react';
-import ResponsiveDrawer from '../components/responsiveDrawer';
 import { withDSXContext } from '../dsxContext.js';
 import { withStyles } from '@material-ui/core/styles';
+import GridListTile from "@material-ui/core/GridListTile";
+import Typography from "@material-ui/core/Typography";
+
 
 const styles = (theme) => ({
-  mainContainer: {
-    'display': 'flex',
-    'flex-direction': 'column',
+  root: {
+    flexGrow: 1,
+    height: '100%',
   },
-  topContainer: {
-    'background': '#ffffff',
-    'display': 'flex',
-    'height': '80%',
-    'width': '100%',
+  panels: {
+    backGround: '#ffffff',
+    border: '1px solid #A9A9A9',
   },
-  verticalDivider: {
-    'background': '#D3D3D3',
-    'width': '4px',
+  gridlistRoot: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
   },
-  horizontalDivider: {
-    'background': '#D3D3D3',
-    'height': '3px',
-
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
   },
-  bottomPanel: {
-    'background': '#ffffff',
-    'height': '20%',
-    'display': 'flex',
-    'overflow': 'auto',
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
 });
 
@@ -94,9 +105,10 @@ class EmbeddingMorseSmaleWindow extends React.Component {
     const { drawerImages } = this.state;
 
     return (
-      <div className={classes.main}>
-        <div className={classes.topContainer}>
-          <div style={{ width:'50%' }}>
+      <React.Fragment>
+        <Grid container style={{ height:'80%' }}>
+          <Grid item xs={6}>
+            {/* <div>*/}
             <EmbeddingWindow
               dataset={this.props.dataset}
               decomposition={this.props.decomposition}
@@ -105,26 +117,87 @@ class EmbeddingMorseSmaleWindow extends React.Component {
               onDesignSelection={this.props.onDesignSelection}
               activeDesigns={this.props.activeDesigns}
               numberOfWindows={this.props.numberOfWindows}/>
-          </div>
-          <div className={classes.verticalDivider}/>
-          <div style={{ width:'50%' }}>
+            {/* </div>*/}
+          </Grid>
+          <Grid item xs={6}>
+            {/* <div>*/}
             <MorseSmaleWindow
               dataset={this.props.dataset}
               decomposition={this.props.decomposition}
               numberOfWindows={this.props.numberOfWindows}
               onCrystalSelection={this.props.onCrystalSelection}
               evalShapeoddsModelForCrystal={this.computeNewSamplesUsingShapeoddsModel}/>
-          </div>
+            {/* </div>*/}
+          </Grid>
+          {/* <Grid item xs={12} style={{ height:'100px' }}>*/}
+          {/*  {drawerImages.length > 0 && drawerImages.map((thumbnail) =>*/}
+          {/*    <img alt={'Image:' + thumbnail.id}*/}
+          {/*      height='45'*/}
+          {/*      width='45'*/}
+          {/*      style={{ margin:'5px 5px 5px 5px' }}*/}
+          {/*      src={'data:image/png;base64, ' + thumbnail.img.rawData}/>)}*/}
+          {/* </Grid>*/}
+        </Grid>
+        <div className={classes.gridlistRoot}>
+          <GridList className={classes.gridList} cols={12}>
+            {drawerImages.map((tile) => (
+            <GridListTile key={tile.id}>
+              <Paper className={classes.paper}>
+                <Typography>{'Design: ' + tile.id}</Typography>
+                <img alt={'Image:' + tile.id}
+                     height={tile.img.height}
+                     width={tile.img.width}
+                     src={'data:image/png;base64, ' + tile.img.rawData}/>
+              </Paper>
+            </GridListTile>))}
+          </GridList>
+          {/*{drawerImages.map((thumbnail) =>*/}
+          {/*  <img alt={'Image:' + thumbnail.id}*/}
+          {/*    height={thumbnail.img.height}*/}
+          {/*    width={thumbnail.img.width}*/}
+          {/*    style={{ margin:'5px 5px 5px 5px' }}*/}
+          {/*    src={'data:image/png;base64, ' + thumbnail.img.rawData}/>)}*/}
         </div>
-        <div className={classes.horizontalDivider}/>
-        <div className={classes.bottomPanel}>
-          {drawerImages.length > 0 && drawerImages.map((thumbnail) =>
-            <span>thumbnail.id</span>)}
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
+//   render() {
+//     const { classes } = this.props;
+//     const { drawerImages } = this.state;
+//
+//     return (
+//       <div className={classes.main}>
+//         <div className={classes.topContainer}>
+//           <div style={{ width:'50%' }}>
+//             <EmbeddingWindow
+//               dataset={this.props.dataset}
+//               decomposition={this.props.decomposition}
+//               embedding={this.props.embedding}
+//               selectedDesigns={this.props.selectedDesigns}
+//               onDesignSelection={this.props.onDesignSelection}
+//               activeDesigns={this.props.activeDesigns}
+//               numberOfWindows={this.props.numberOfWindows}/>
+//           </div>
+//           <div className={classes.verticalDivider}/>
+//           <div style={{ width:'50%' }}>
+//             <MorseSmaleWindow
+//               dataset={this.props.dataset}
+//               decomposition={this.props.decomposition}
+//               numberOfWindows={this.props.numberOfWindows}
+//               onCrystalSelection={this.props.onCrystalSelection}
+//               evalShapeoddsModelForCrystal={this.computeNewSamplesUsingShapeoddsModel}/>
+//           </div>
+//         </div>
+//         <div className={classes.horizontalDivider}/>
+//         <div className={classes.bottomPanel}>
+//           {drawerImages.length > 0 && drawerImages.map((thumbnail) =>
+//             <span>thumbnail.id</span>)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default withDSXContext(withStyles(styles)(EmbeddingMorseSmaleWindow));
 
