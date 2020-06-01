012eb112a5333eaaa19b7ee21f915bb75027cc82
Cameron Christensen <cam@sci.utah.edu>
2020-05-31 21:18:56 -0600

    cleanup hackathon and start merge with drawer branch

--- client/src/windows/embeddingMorseSmaleWindow.js
+++ client/src/windows/embeddingMorseSmaleWindow.js
@@ -9,47 +9,21 @@ import { withStyles } from '@material-ui/core/styles';
 import SizeMonitor from './sizeMonitor';
 import GalleryWindow from './galleryWindow';
 
-const drawerHeight = 200;
+const drawerHeight = 250;
 const styles = (theme) => ({
   embeddingMorseSmaleWorkspace: {
     background:'#ffffff',
     display:'flex',
     flexDirection:'column',
     height:'100%',
-    //height:'100vh',
-    //height:'calc(100vh - 200px)',               // holy cow this keeps things consistent! Too small top panels, but still consistent.
-    //height:'calc(100vh - '+drawerHeight+'px)',    // ...and this lets me specify vars
-                                                  // seems like vh is fixed at the start, but wrong, and when size changes it doesn't
-                                    // ...and expands top items, maybe flex-end? yes, but that's good. Just need to have something like vh that works dynamically
-    // minHeight: 'calc(100vh - '+drawerHeight+'px)',  // same as just height:'calc...
-    // maxHeight: 'calc(100vh - '+drawerHeight+'px)',
-    //height:'calc(1fr - '+drawerHeight+'px)',    // ...but this doesn't work, back to expanding
-    //height:'calc(100% - '+drawerHeight+')',      // starts good, but still gets bigger on expanding window size
-    //height:'1000',             // doesn't make a difference to have height of any sort
-    // minHeight: 1000,  // doesn't make a difference to have min/max -- wait! It made a difference to have it be small (I accidentally tried drawerHeight)
-    // maxHeight: 1000,
-    // minHeight: 'calc(1fr - '+drawerHeight+')',  // 1fr doesn't make a difference
-    // maxHeight: 'calc(1fr - '+drawerHeight+')',
-    // minHeight: 'calc(100% - '+drawerHeight+')',  // 100% doesn't make a difference
-    // maxHeight: 'calc(100% - '+drawerHeight+')',
-    // minHeight: 'calc(100vh - '+drawerHeight+')',  // 100vh doesn't make a difference
-    // maxHeight: 'calc(100vh - '+drawerHeight+')',
-    // minHeight: 'calc('+drawerHeight+')',  // just using drawerHeight doesn't make a difference, so probably the calc doesn't work
-    // maxHeight: 'calc('+drawerHeight+')',
-    // height:`1fr-drawerHeight`,   // no difference
-    // minHeight:`1fr-drawerHeight`,
-    // maxHeight:`1fr-drawerHeight`,
-    justifyContent:'flex-end',  // doesn't make a difference from space-evenly
+    justifyContent:'flex-end',
   },
   topPanels: {
-    flex:'auto',  // fills space, respects as window size is minimized, but always grows when size increases past initial size. Getting closer...
-                    // removing auto starts with smaller top panels if window is wide, but adjusts vertical size on resize in both directions.
-    //flex:'0 0 1000',  // nothing here makes a different, 1 1 auto is just like auto
+    flex:'auto',
     display:'flex',
     flexDirection:'row',
-    //justifyContent:'space-evenly',
-    height:'calc(100vh - '+drawerHeight+'px)',    // ...and this lets me specify vars
-    justifyContent:'flex-end',  // doesn't make a difference from space-evenly
+    height:'calc(100vh - '+drawerHeight+'px)',
+    justifyContent:'flex-end',
   },
   verticalDivider: {
     background:'#808080',
@@ -68,48 +42,19 @@ const styles = (theme) => ({
     width:'100%',
     height:'5px',
   },
-  drawer: {
-    height:'20%',
-    display:'grid',
-    gridTemplateColumns:'repeat(auto-fill, minmax(45px, 1fr))',
-    gridTemplateRows:'1fr 100px',
-    gap:'1rem',
-    overflow:'hidden',
-  },
-  flexdrawer: {
-    display:'flex',
-    height:'100px',
-    flexDirection:'row',
-    overflow:'hidden',
-  },
-  drawerItem: {
-    width:'auto',
-  },
-  paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    border: 'solid #D3D3D3'
-  },
-
-  //from appication (since it manages to create a scrollable gallery)
   workspace: {  
     display: 'grid',
-    //height: 'calc(100vh - 64px)',
-    //height:'200px',
     height: drawerHeight,
-    minHeight: drawerHeight,  // doesn't make a difference to have min/max
-    maxHeight: drawerHeight,
     gridTemplateColumns: '1fr',
     gridTemplateRows: '1fr',
     gridGap: '0em',
   },
-  // content: {
-  //   flexGrow: 1,
-  //   minWidth: 0,
-  //   backgroundColor: '#eef',
-  //   position: 'relative',
-  // },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'top',
+    border: 'solid #D3D3D3',
+  },
 });
 
 /**
@@ -207,153 +152,29 @@ class EmbeddingMorseSmaleWindow extends React.Component {
           {/* drawer divider (todo: onMouseDown={this.onResizeDrawer})*/}
           <div className={classes.drawerDivider} />
 
-          {/* shape cards drawer */}
-        <div className={classes.workspace} >
+          {/* scrollable shape cards drawer */}
+          <div className={classes.workspace} >
+            <Paper style={{ overflow:'hidden auto', border:'1px solid gray' }}>
+              <Grid container
+                    justify={'flex-start'}
+                    spacing={8}
+                    style={{ margin:'5px 0px 0px 0px' }}>
+                {drawerImages.map((tile, i) =>
 
-      {/* very old but still extant issue, fix here: https://stackoverflow.com/a/55751683/207044 (probably need to use standard grid, not material-ui grid */}
-              <Paper style={{ overflow:'hidden auto', border:'1px solid gray' }}>
-                <Grid container
-                      justify={'flex-start'}
-                      spacing={8}
-                      style={{ margin:'5px 0px 0px 0px' }}>
-                  {drawerImages.map((tile, i) =>
-
-                  <Grid key={i} item>
-                    <Paper
-                      style={{ backgroundColor:'#FFA500' }}>
-                      <img alt={'Image:' + tile.id} key={i}
-                           height='45'
-                           style={{ margin:'5px 5px 5px 5px' }}
-                           src={'data:image/png;base64, ' + tile.img.rawData}/>
-                    </Paper>
-                  </Grid>)}
-                </Grid>
-              </Paper>
+                <Grid key={i} item>
+                  <Paper className={classes.paper}>
+                    <img alt={'Image:' + tile.id} key={i}
+                         height='45'
+                         style={{ margin:'5px 5px 5px 5px' }}
+                         src={'data:image/png;base64, ' + tile.img.rawData}/>
+                  </Paper>
+                </Grid>)}
+              </Grid>
+            </Paper>
+          </div>
         </div>
-      </div>
     );
   }
 }
-
-// works, but unscrollable
-          // {/* shape cards drawer */}
-          //     <Paper style={{ overflow:'hidden auto', border:'1px solid gray', height:'20%' }}>
-          //       <Grid container
-          //             justify={'flex-start'}
-          //             spacing={8}
-          //             style={{ margin:'5px 0px 0px 0px' }}>
-          //         {drawerImages.map((tile, i) =>
-
-          //         <Grid key={i} item>
-          //           <Paper
-          //             style={{ backgroundColor:'#FFA500' }}>
-          //             <img alt={'Image:' + tile.id} key={i}
-          //                  height='45'
-          //                  style={{ margin:'5px 5px 5px 5px' }}
-          //                  src={'data:image/png;base64, ' + tile.img.rawData}/>
-          //           </Paper>
-          //         </Grid>)}
-          //       </Grid>
-          //     </Paper>
-
-//argh! just going to use gallery instead
-        // <div className={classes.flexdrawer} >
-        //   {drawerImages.map((tile, i) =>
-        //                     <img alt={'Image:' + tile.id} key={i}
-        //            height='45'
-        //            style={{ margin:'5px 5px 5px 5px' }}
-        //                     src={'data:image/png;base64, ' + tile.img.rawData}/>)};
-
-
-//still problematic: no scrolling
-                        // <GalleryWindow style={{ height:'20%', flex:'auto' }} 
-                        //   dataset={this.props.dataset}
-                        //   selectedDesigns={this.props.selectedDesigns}
-                        //   onDesignSelection={this.onDesignSelection}
-                        //   activeDesigns={this.props.activeDesigns}/>);
-
-//handy test which finally helped identify ReactResizeDetector as the culprit for all this crap (well, maybe... yes, I double checked.)
-        // <div style={{ width:'100%', height:'100%', background:'#ffffff' }}>
-        //   <SizeMonitor />
-        // </div>
-
-// responsivedrawer back and works just like before
-        // {/* shape cards drawer */}
-        // <div style={{ height:'20%', flex:'auto', display:'flex', flexDirection:'row', flexWrap:'nowrap', overflow:'hidden' }}>  {/* justifyContent:'space-around',  */}
-        //   <ResponsiveDrawer images={this.state.drawerImages}/>
-        // </div>
-
-// going to try to bring back responsivedrawer
-        // {/* shape cards drawer */}
-        // <div style={{ height:'20%', flex:'auto', display:'flex', flexDirection:'row', flexWrap:'nowrap', overflow:'hidden' }}>  {/* justifyContent:'space-around',  */}
-        //   {drawerImages.map((tile) => (
-        //   <div key={tile.id} style={{ height:'50px', height:'50px', flex:'auto' }} >
-        //     <img alt={'Image:' + tile.id}
-        //          src={'data:image/png;base64, ' + tile.img.rawData} />
-        //   </div>
-        //   ))}
-        // </div>
-
-//unfortunately, this still results in incorrect [increasing height] sizes on update. 
-      // <div style={{ width:'100%', height:'100%', background:'#ffffff', display:'flex', flexDirection:'column' }}>
-
-      //   {/* top panel: embedding and crystals */}
-      //   <div style={{ height:'100%', flex:'auto', display:'flex', flexDirection:'row' }}>
-
-      //     {/* embedding */}
-      //     <div style={{ width:'100%', flex:'auto' }}>
-      //       <EmbeddingWindow
-      //         dataset={this.props.dataset}
-      //         decomposition={this.props.decomposition}
-      //         embedding={this.props.embedding}
-      //         selectedDesigns={this.props.selectedDesigns}
-      //         onDesignSelection={this.props.onDesignSelection}
-      //         activeDesigns={this.props.activeDesigns}
-      //         numberOfWindows={this.props.numberOfWindows}/>
-      //     </div>
-
-      //     {/* embedding/crystals vertical divider */}
-      //     <div style={{ background:'#808080', width:'5px', height:'100%' }} />
-
-      //     {/* crystals */}
-      //     <div style={{ width:'100%', flex:'auto' }}>
-      //   {/*
-      //       <MorseSmaleWindow
-      //         dataset={this.props.dataset}
-      //         decomposition={this.props.decomposition}
-      //         numberOfWindows={this.props.numberOfWindows}
-      //         onCrystalSelection={this.props.onCrystalSelection}
-      //         evalShapeoddsModelForCrystal={this.computeNewSamplesUsingShapeoddsModel}/>
-      //    */}
-      //     </div>
-      //   </div>
-
-      //   {/* drawer divider (todo: onMouseDown={this.onResizeDrawer})*/}
-      //   <div style={{ background:'#808080', width:'100%', height:'5px' }} />
-
-      //   {/* shape cards drawer */}
-      //   <div style={{ height:'20%', flex:'auto', display:'flex', flexDirection:'row', flexWrap:'nowrap' }}>  {/* justifyContent:'space-around', overflow:'hidden' */}
-      //     {drawerImages.map((tile) => (
-      //     <div key={tile.id} style={{ width:'50px', flex:'auto' }} >
-      //       <img alt={'Image:' + tile.id}
-      //            src={'data:image/png;base64, ' + tile.img.rawData} />
-      //     </div>
-      //     ))}
-      //   </div>
-      // </div>
-
-// this works as last item right before end of ResizablePanels, now trying a gridlist
-      //</div>
-        // <div style={{ background:'#ffffff', height:'100%', width:'100%' }}>
-        //     <EmbeddingWindow
-        //       dataset={this.props.dataset}
-        //       decomposition={this.props.decomposition}
-        //       embedding={this.props.embedding}
-        //       selectedDesigns={this.props.selectedDesigns}
-        //       onDesignSelection={this.props.onDesignSelection}
-        //       activeDesigns={this.props.activeDesigns}
-        //       numberOfWindows={this.props.numberOfWindows}/>
-        // </div>
-      //</ResizablePanels>
 
 export default withDSXContext(withStyles(styles) (EmbeddingMorseSmaleWindow));
