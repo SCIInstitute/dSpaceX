51cdca728aad698794bec7c20255ec5a01c2d194
Kyli McKay-Bishop <kyli.nmb@gmail.com>
2020-08-07 14:20:02 -0600

    volume thumbnails generation not working for some volumes. Created file to test one off scenarios

--- data/thumbnails/volume_thumbnails.py
+++ data/thumbnails/volume_thumbnails.py
@@ -3,7 +3,7 @@ import nrrd
 import re
 from skimage import measure
 
-from thumbnails.thumbnail_utils import generate_image_from_vertices_and_faces
+from data.thumbnails.thumbnail_utils import generate_image_from_vertices_and_faces
 
 
 def generate_volume_thumbnails(shape_directory, output_directory, scale=10):
@@ -13,16 +13,17 @@ def generate_volume_thumbnails(shape_directory, output_directory, scale=10):
     # For each mesh format generate thumbnail
     for index, shape_file in enumerate(shapes):
         shape_id = list(map(int, re.findall(r'\d+', shape_file)))[-1]
+        print(shape_id)
         print('Thumbnail generation %.2f percent complete. Generating thumbnail %i of %i. ' %
               ((100 * index / len(shapes)), index, len(shapes)), end='\r')
 
         data, _ = nrrd.read(shape_file)
-        vertices, faces, _, _, = measure.marching_cubes(data)
+        vertices, faces, _, _, = measure.marching_cubes(data, .75)
         vertices = vertices / scale
         image = generate_image_from_vertices_and_faces(vertices, faces)
         image.save(output_directory + str(shape_id) + '.png')
 
 
-in_dir = '/Users/kylimckay-bishop/Temporary/OriginalVolume/'
-out_dir = '/Users/kylimckay-bishop/Temporary/thumbnails2/'
-generate_mesh_thumbnails(in_dir, out_dir)
+# in_dir = '/Volumes/External_Drive/dSpaceX_data/raw_data/nanoparticles/highres/OriginalVolume/'
+# out_dir = '/Users/kylimckay-bishop/Temporary/thumbnails_old_volumes/'
+# generate_volume_thumbnails(in_dir, out_dir)
