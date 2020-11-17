d37ae6889a2799a6f361add56c6da3277cf234f6
Cameron Christensen <cam@sci.utah.edu>
2020-11-17 15:17:37 -0700

    fix binarize_models Python function; add example notebook for using binarize functions to binarize existing processed datasets

--- data/convert/binarize.py
+++ data/convert/binarize.py
@@ -44,12 +44,15 @@ def binarize_models(model_dir, dtype = np.float32):
         print('Model binarization %.2f percent complete (%s elapsed). Converting file %i of %i. ' %
               ((100 * index / len(csvfiles)), timedelta(seconds=elapsed), index, len(csvfiles)), end='\r')
 
-        df = pd.read_csv(infile, header=None, dtype=dtype)
-        outfile = os.path.splitext(infile)[0] + ".bin"
-        #print(infile + " to " + outfile + " (shape: " + str(df.shape) + ")")
-        np.asarray(df.values).tofile(outfile)
-        dims = open(outfile + ".dims", 'w')
-        dims.write(str(df.shape[0]) + ' ' + str(df.shape[1]) + ' ' + "float32")
+        try:
+            df = pd.read_csv(infile, header=None, dtype=dtype)
+            outfile = os.path.splitext(infile)[0] + ".bin"
+            #print(infile + " to " + outfile + " (shape: " + str(df.shape) + ")")
+            np.asarray(df.values).tofile(outfile)
+            dims = open(outfile + ".dims", 'w')
+            dims.write(str(df.shape[0]) + ' ' + str(df.shape[1]) + ' ' + "float32")
+        except:
+            print("exception trying to read " + infile + ". Skipping.")
 
     print('', end='\n')
     print("done!")
