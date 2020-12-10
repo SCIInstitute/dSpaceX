e45c73f981b57d162a3c1dc5ff4a30f3a5e29969
Cameron Christensen <cam@sci.utah.edu>
2020-12-10 07:51:44 -0700

    index thumbnails more simply

--- data/thumbnails/mesh_thumbnails.py
+++ data/thumbnails/mesh_thumbnails.py
@@ -21,13 +21,12 @@ def generate_mesh_thumbnails(shape_directory, output_directory, resolution = [30
 
     # For each mesh format generate thumbnail
     for index, shape_file in enumerate(shapes):
-        shape_id = list(map(int, re.findall(r'\d+', shape_file)))[-1]
         print('Thumbnail generation %.2f percent complete. Generating thumbnail %i of %i. ' %
               ((100 * index / len(shapes)), index, len(shapes)), end='\r')
 
         ren.loadNewMesh(shape_file)
         image = PIL.Image.fromarray(ren.getImage(resolution, scale))
-        filename = path.join(output_directory, format(shape_id, padding) + '.png')
+        filename = path.join(output_directory, format(index+1, padding) + '.png')
         image.save(filename)
         
     print('', end='\n')
